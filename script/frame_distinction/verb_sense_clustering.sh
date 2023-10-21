15e08decd4a8b4b9187f246de8241800e9de8c71
Kosuke-Yamada <ko5su2ke6_fykn@icloud.com>
2023-10-21 19:55:51 +0900

    change pathes

--- script/frame_distinction/verb_sense_clustering.sh
+++ script/frame_distinction/verb_sense_clustering.sh
@@ -3,10 +3,10 @@
 resource=framenet
 #resource=propbank
 
-model_name=bert-base-uncased
+# model_name=bert-base-uncased
 #model_name=albert-base-v2
 #model_name=roberta-base
-#model_name=gpt2
+model_name=gpt2
 #model_name=xlnet-base-cased
 layers=(0 1 2 3 4 5 6 7 8 9 10 11 12)
 
@@ -20,10 +20,10 @@ layers=(0 1 2 3 4 5 6 7 8 9 10 11 12)
 # layers=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24)
 
 for resource in framenet propbank; do
-    for layer in ${layers}; do
-        python verb_sense_clustering.py \
+    for layer in ${layers[@]}; do
+        python ../experiment_frame_distinction/verb_sense_clustering.py \
             --input_path ../data/experiment_frame_distinction/embeddings \
-            --output_path ../data/experiment_frame_distinction/visualization \
+            --output_path ../data/experiment_frame_distinction/frame_distinction \
             --resource ${resource} \
             --model_name ${model_name} \
             --layer ${layer} \
@@ -32,9 +32,10 @@ for resource in framenet propbank; do
             --sets dev
     done
 
-    python verb_sense_clustering.py \
+    layer=-1
+    python ../experiment_frame_distinction/verb_sense_clustering.py \
         --input_path ../data/experiment_frame_distinction/embeddings \
-        --output_path ../data/experiment_frame_distinction/visualization \
+        --output_path ../data/experiment_frame_distinction/frame_distinction \
         --resource ${resource} \
         --model_name ${model_name} \
         --layer ${layer} \
